a420bc7000ea95c1e9b177849180d144ba1af002
Morgan <nicol.morgan@outlook.fr>
2019-01-31 18:42:04 +0100

    Relou btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'a-button';
+export class AppComponent implements OnInit{
+
+  relouIncrement;
+
+  ngOnInit() {
+  }
+
+  clickRelou() {
+    console.log('e')
+  }
 }
